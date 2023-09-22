17e62e77e0c360c459c317d3be425c09a0c161d5
Juan Cruz <membrillajc@gmail.com>
2023-09-21 21:22:32 -0300

    Preparando para hacer pull

--- apps/api/src/vehiculos/vehiculos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VehiculosController } from './vehiculos.controller';
-import { VehiculosService } from './vehiculos.service';
-
-@Module({
-  controllers: [VehiculosController],
-  providers: [VehiculosService]
-})
-export class VehiclesModule {}
